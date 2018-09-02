b99004084093055a65be8a73489bb57dc14f8563
John <johnsmilga@gmail.com>
2018-09-01 23:03:27 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// immediate invoked function expression
+
+(function() {
+  const pictures = [
+    "contBcg-0",
+    "contBcg-1",
+    "contBcg-2",
+    "contBcg-3",
+    "contBcg-4"
+  ];
+})();
